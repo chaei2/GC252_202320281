25fe8c8920afb9c3ad6c10742bca5ceaf09af628
chaei <209975817+chaei2@users.noreply.github.com>
2025-10-12 04:10:20 +0900

    깜빡이는게 이상하게 안되네..;음

---
+++ source/blinking.js
@@ -0,0 +1,13 @@
+// 음 왜 안되는거지...ㅎ...
+const textElement = document.querySelector('.blinking-text');
+const text = textElement.textContent;
+let blinkingText = '';
+
+for (let cnt = 0; cnt < text.length; cnt++) {
+  const randomDelay = Math.random() * 2;
+  const charSpan = `<span style="animation-delay: ${randomDelay}s;">${text[cnt]}</span>`;
+
+  blinkingText += charSpan;
+}
+
+textElement.innerHTML = blinkingText;
