cd4f9f96f79031434d25f57dcb58b87de5f52479
chaei <209975817+chaei2@users.noreply.github.com>
2025-10-16 11:37:57 +0900

    템플릿 좀 옮김

--- source/blinking.js
+++ source/blinking.js
@@ -1,13 +1,13 @@
 // 음 왜 안되는거지...ㅎ...
-const textElement = document.querySelector('.blinking-text');
-const text = textElement.textContent;
-let blinkingText = '';
+// const textElement = document.querySelector('.blinking-text');
+// const text = textElement.textContent;
+// let blinkingText = '';
 
-for (let cnt = 0; cnt < text.length; cnt++) {
-  const randomDelay = Math.random() * 2;
-  const charSpan = `<span style="animation-delay: ${randomDelay}s;">${text[cnt]}</span>`;
+// for (let cnt = 0; cnt < text.length; cnt++) {
+//   const randomDelay = Math.random() * 2;
+//   const charSpan = `<span style="animation-delay: ${randomDelay}s;">${text[cnt]}</span>`;
 
-  blinkingText += charSpan;
-}
+//   blinkingText += charSpan;
+// }
 
-textElement.innerHTML = blinkingText;
+// textElement.innerHTML = blinkingText;
