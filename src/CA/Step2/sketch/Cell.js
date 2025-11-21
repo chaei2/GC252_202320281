9f01b8f55f5a0c198d526e05ec72f07895282fce
chaei <209975817+chaei2@users.noreply.github.com>
2025-11-21 11:52:35 +0900

    완료

--- src/CA/Step2/sketch/Cell.js
+++ src/CA/Step2/sketch/Cell.js
@@ -3,36 +3,46 @@ class Cell {
   size = [0, 0];
   state = false;
   nextState = false;
-  neighbors = [null, null];
-  rule = "00010011";
+  // 이웃은 확장되어야 함 8개로
+  neighbors = [null, null, null, null, null, null, null, null];
+
+  // 룰은 함수로 만들어야 함 일단 삭제
+  // rule = '00010011';
 
   constructor(x, y, w, h, state = false) {
     this.pos = [x, y];
     this.size = [w, h];
     this.state = state;
   }
-
-  setNeighbors(left, right) {
-    this.neighbors[0] = left;
-    this.neighbors[1] = right;
+  // T, B, R, L(시계 방향으로 감아지는게 나음)
+  setNeighbors(tl, t, tr, r, br, b, bl, l) {
+    this.neighbors[0] = tl;
+    this.neighbors[1] = t;
+    this.neighbors[2] = tr;
+    this.neighbors[3] = r;
+    this.neighbors[4] = br;
+    this.neighbors[5] = b;
+    this.neighbors[6] = bl;
+    this.neighbors[7] = l;
   }
-
-  getCombinedState() {
-    const binaryString = `${this.neighbors[0]?.state ? 1 : 0}${
-      this.state ? 1 : 0
-    }${this.neighbors[1]?.state ? 1 : 0}`;
-    const decimalNum = parseInt(binaryString, 2);
-    return decimalNum;
-  }
-
-  getNextState() {
-    const combinedState = this.getCombinedState();
-    const nextStateString = this.rule.charAt(7 - combinedState);
-    return nextStateString === "1";
-  }
-
   computeNextState() {
-    this.nextState = this.getNextState();
+    // filter();는 내장 되어있는 함수
+    // .length는 몇인가?
+    const livingCnt = this.neighbors.filter((aNeighbor) => {
+      return aNeighbor?.state;
+    }).length;
+
+    // 2랑 3은 여전히 비어있음 3도 해결함(3번은 무조건 살아있는 상태임)
+    // else는 내가 정확히 2인 순간임
+    if (livingCnt < 2) {
+      this.nextState = false;
+    } else if (livingCnt > 3) {
+      this.nextState = false;
+    } else if (livingCnt === 3) {
+      this.nextState = true;
+    } else {
+      this.nextState = this.state;
+    }
   }
 
   updateState() {
@@ -54,7 +64,7 @@ class Cell {
 
   render(isHovered = false) {
     strokeWeight(2);
-    stroke(isHovered ? "red" : 200);
+    stroke(isHovered ? 'red' : 200);
     if (this.state) {
       fill(0);
     } else {
--- src/CA/Step2/sketch/main.js
+++ src/CA/Step2/sketch/main.js
@@ -1,55 +1,46 @@
-const canvasContainer = document.getElementById("canvas-container");
+const canvasContainer = document.getElementById('canvas-container');
 let renderer;
+// canvas
 
 const INITIAL_W = 800;
 const INITIAL_H = 600;
 const INITIAL_RATIO = INITIAL_W / INITIAL_H;
 
-const cellsPerRow = 20;
+// cellsPerRow 한 줄에 몇 개의 칸을 넣을 것인가?-> 80이면 한 칸당 10이 나옴
+const cellsPerRow = 80;
 let cellsPerColumn;
 const cells = [];
 let cellSize;
 
 let hoveredCell = null;
 
-function createNextGen() {
-  const refIdx = cells.length - cellsPerRow;
-  for (let idx = refIdx; idx < refIdx + cellsPerRow; idx++) {
-    cells[idx].computeNextState();
-  }
-  for (let n = 0; n < cellsPerRow; n++) {
-    const refCell = cells[refIdx + n];
-    const x = width * 0.5 - 0.5 * cellSize * cellsPerRow + n * cellSize;
-    const y = refCell.pos[1] + cellSize;
-    const w = cellSize;
-    const h = cellSize;
-    const cell = new Cell(x, y, w, h, refCell.nextState);
-    cells.push(cell);
-  }
-  for (let n = 0; n < cellsPerRow; n++) {
-    const idx = refIdx + cellsPerRow + n;
-    const left = n === 0 ? null : cells[idx - 1];
-    const right = n === cellsPerRow - 1 ? null : cells[idx + 1];
-    cells[idx].setNeighbors(left, right);
-  }
+// function updateAllGenerations() {
+//   const currentRowCnt = Math.floor(cells.length / cellsPerRow);
+//   for (let row = 0; row < currentRowCnt; row++) {
+//     for (let col = 0; col < cellsPerRow; col++) {
+//       const idx = row * cellsPerRow + col;
+//       if (row !== 0) {
+//         const refIdx = (row - 1) * cellsPerRow + col;
+//         cells[idx].state = cells[refIdx].nextState;
+//       }
+//     }
+//     for (let col = 0; col < cellsPerRow; col++) {
+//       const idx = row * cellsPerRow + col;
+//       cells[idx].computeNextState();
+//     }
+//   }
+// }
+
+function getIdx(r, c) {
+  return r * cellsPerRow + c;
 }
 
-function updateAllGenerations() {
-  const currentRowCnt = Math.floor(cells.length / cellsPerRow);
-  for (let row = 0; row < currentRowCnt; row++) {
-    for (let col = 0; col < cellsPerRow; col++) {
-      const idx = row * cellsPerRow + col;
-      if (row !== 0) {
-        const refIdx = (row - 1) * cellsPerRow + col;
-        cells[idx].state = cells[refIdx].nextState;
-      }
-    }
-    for (let col = 0; col < cellsPerRow; col++) {
-      const idx = row * cellsPerRow + col;
-      cells[idx].computeNextState();
-    }
-  }
-}
+let cnt = 0;
+
+// 절대 시간에 의존하는 법
+// millis(), lastTime, lntervul
+let lastTime = 0;
+const interval = 1000;
 
 function setup() {
   renderer = createCanvas(INITIAL_W, INITIAL_H);
@@ -66,35 +57,74 @@ function setup() {
   cellSize = width / cellsPerRow;
   cellsPerColumn = Math.floor(height / cellSize);
 
-  for (let n = 0; n < cellsPerRow; n++) {
-    const x = width * 0.5 - 0.5 * cellSize * cellsPerRow + n * cellSize;
-    const y = 0;
-    const w = cellSize;
-    const h = cellSize;
-    const cell = new Cell(x, y, w, h, Math.floor(cellsPerRow / 2) === n);
-    cells.push(cell);
+  // 안에 있는게 더 촘촘하게 가기에 가로로하는 row를 안에 넣는다
+
+  // index = w * r + c
+
+  for (let r = 0; r < cellsPerColumn; r++) {
+    for (let c = 0; c < cellsPerRow; c++) {
+      const x = c * cellSize;
+      const y = r * cellSize;
+      const randomState = random() < 0.1;
+      const newCell = new Cell(x, y, cellSize, cellSize, randomState);
+      cells.push(newCell);
+    }
   }
 
+  // 젤 위에 있냐 아래 있냐, 왼 오른을 결정해줘야함
+  // index = w * r + c
+  // 8을 8ㄹ로 나누면 1이라는 숫자가 뜸
   cells.forEach((cell, idx) => {
-    const left = cells[idx - 1];
-    const right = cells[idx + 1];
-    // console.log(idx, left, right);
-    cell.setNeighbors(left, right);
+    const row = Math.floor(idx / cellsPerRow);
+    const col = idx % cellsPerRow;
+    const tl = row > 0 && col > 0 ? cells[getIdx(row - 1, col - 1)] : null;
+    const t = row > 0 ? cells[getIdx(row - 1, col)] : null;
+    const tr =
+      row > 0 && col < cellsPerRow - 1 ? cells[getIdx(row - 1, col + 1)] : null;
+    const r = col < cellsPerRow - 1 ? cells[getIdx(row, col + 1)] : null;
+    const br =
+      row < cellsPerColumn - 1 && col < cellsPerRow - 1
+        ? cells[getIdx(row + 1, col + 1)]
+        : null;
+    const b = row < cellsPerColumn - 1 ? cells[getIdx(row + 1, col)] : null;
+    const bl =
+      row < cellsPerColumn - 1 && col > 0
+        ? cells[getIdx(row + 1, col - 1)]
+        : null;
+    const l = col > 0 ? cells[getIdx(row, col - 1)] : null;
+    cell.setNeighbors(tl, t, tr, r, br, b, bl, l);
   });
 
-  for (let row = 1; row < cellsPerColumn; row++) {
-    createNextGen();
-  }
+  // 연산이 1초에 한 번만 일어남
+  // frameRate();
 }
 
 function draw() {
+  // cnt--;
   background(250);
+  cells.forEach((aCell) => {
+    aCell.computeNextState();
+  });
+
+  if (millis() - lastTime > interval) {
+    cells.forEach((aCell) => {
+      aCell.updateState();
+    });
+    lastTime = millis();
+  }
+
   cells.forEach((cell) => cell.render(cell === hoveredCell));
+
+  // noStroke();
+  // fill('red');
+  // textSize(50);
+  // textAlign(CENTER, CENTER);
+  // text(cnt, width * 0.5, height * 0.5);
 }
 
 function mouseMoved() {
   hoveredCell = null;
-  for (let idx = 0; idx < cellsPerRow; idx++) {
+  for (let idx = 0; idx < cell.length; idx++) {
     if (cells[idx].isHovered(mouseX, mouseY)) {
       hoveredCell = cells[idx];
       break;
@@ -105,15 +135,4 @@ function mouseMoved() {
 
 function mousePressed() {
   hoveredCell?.toggleState();
-  console.log(hoveredCell?.getCombinedState());
-  console.log(hoveredCell?.getNextState());
-  updateAllGenerations();
-}
-
-function keyPressed() {
-  if (key === "s" || key === "S") {
-    createNextGen();
-  } else if (key === "w" || key === "W") {
-    cells.length = cells.length - cellsPerRow;
-  }
 }
