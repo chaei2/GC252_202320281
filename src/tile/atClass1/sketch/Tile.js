7aaae39fc6119f8a7594ee61814a834f67299514
chaei <209975817+chaei2@users.noreply.github.com>
2025-11-28 11:41:45 +0900

    중간과정

---
+++ src/tile/atClass1/sketch/Tile.js
@@ -0,0 +1,64 @@
+class Tile {
+  pos = [0, 0];
+  // size는 0, 0으로 만들면 문제가 될 수 있음
+  size = [1, 1];
+
+  // t, l, b, r반시계
+  neighbors = [null, null, null, null];
+  state = false;
+  tileImgIdx = 0;
+
+  constructor(x, y, w, h) {
+    this.pos[0] = x;
+    this.pos[1] = y;
+    this.size[0] = w;
+    this.size[1] = h;
+  }
+
+  setNeighbor(t, l, b, r) {
+    this.neighbors[0] = t;
+    this.neighbors[1] = l;
+    this.neighbors[2] = b;
+    this.neighbors[3] = r;
+  }
+
+  computeState() {
+    let binaryString = '';
+
+    this.neighbors.forEach((aNeighbor) => {
+      // if (aNeighbor) {
+      //   // true면 1 넣을게, false면 0 넣을게임
+      //   binaryString += aNeighbor.state ? '1' : '0';
+      // } else {
+      //   binaryString += 0;
+      // }
+
+      binaryString += aNeighbor?.state ? '1' : '0';
+    });
+    // 이진수로 변환해주는 코드
+    this.tileImgIdx = parseInt(binaryString, 2);
+  }
+
+  isHovered(mx, my) {
+    return (
+      mx >= this.pos[0] &&
+      mx <= this.pos[0] + this.size[0] &&
+      my >= this.pos[1] &&
+      my <= this.pos[1] + this.size[1]
+    );
+  }
+
+  toggleState() {
+    this.state = !this.state;
+  }
+
+  render(tiles) {
+    image(
+      tiles[this.tileImgIdx],
+      this.pos[0],
+      this.pos[1],
+      this.size[0],
+      this.size[1]
+    );
+  }
+}
---
+++ src/tile/atClass1/sketch/main.js
@@ -0,0 +1,90 @@
+const canvasContainer = document.getElementById('canvas-container');
+// 혹은 qureySeletor가 있음
+// console.log(canvasContainer) 디버깅;
+let renderer;
+
+const tileImgs = [];
+
+const tiles = [];
+const tilePerRow = 10;
+let tileSize;
+let tilePerCol;
+
+// 전역변수와 지역변수가 있음(광역변수)
+// 오버라이딩하는 경우가 있음, preload
+
+// 일단 하나를 선공한 다음 어래이로 돌리기
+
+const INITIAL_W = 600;
+const INITIAL_H = 300;
+const INITIAL_RATIO = INITIAL_W / INITIAL_H;
+
+function preload() {
+  for (let n = 0; n < 16; n++) {
+    // 백틱의 장점: 그 자리를 변수화 시키고 싶은거잖음? 그러면 저렇게 하면 됨
+    const urlString = `./assets/${String(n).padStart(2, '0')}.svg`;
+    // console.log(urlString);
+    tileImgs.push(loadImage(urlString));
+  }
+}
+
+function tileIdx(col, row) {
+  return row * tilePerRow + col;
+}
+
+// 맥 키보드 백콤 쓰는법-> 한영키 + ₩키 동시에 누름됨
+function setup() {
+  // const renderer = createCanvas(INITIAL_W, INITIAL_H);
+  render = createCanvas(INITIAL_W, INITIAL_H);
+  render.parent(canvasContainer);
+  render.elt.style.aspectRatio = `${INITIAL_W} / ${INITIAL_H}`;
+
+  new ResizeObserver(() => {
+    const { width: containerWidth, height: containerHeight } =
+      canvasContainer.getBoundingClientRect();
+    render.elt.style.width = `${containerWidth}px`;
+    render.elt.style.height = `${containerWidth / INITIAL_RATIO}px`;
+  }).observe(canvasContainer);
+  // resize
+
+  console.log(tileImgs[0]);
+
+  // 타일 사이즈 관한
+  tileSize = width / tilePerRow;
+  tilePerCol = Math.floor(height / tileSize);
+
+  // 가로로가는 for 구문이 있는데, row를 바깥쪽에 설정하고, col을 인쪽에 해줌
+  for (let r = 0; r < tilePerCol; r++) {
+    for (let c = 0; c < tilePerRow; c++) {
+      const x = c * tileSize;
+      const y = r * tileSize;
+      const newTlile = new Tile(x, y, tileSize, tileSize);
+      tiles.push(newTlile);
+    }
+  }
+
+  // idx를 받아야, x로는 몇 번 이고, y로는 몇 번인지 알 수 있음
+  tiles.forEach((aTile, idx) => {
+    const col = idx % tilePerRow;
+    const row = Math.floor(idx / tilePerRow);
+
+    const t = row > 0 ? tiles[tileIdx(col, row - 1)] : null;
+    const l = col > 0 ? tiles[tileIdx(col - 1, row)] : null;
+    const b = row < tilePerCol - 1 ? tiles[tileIdx(col, row + 1)] : null;
+    const r = col < tilePerRow - 1 ? tiles[tileIdx(col + 1, row)] : null;
+
+    aTile.setNeighbor(t, l, b, r);
+  });
+}
+
+function draw() {
+  background(200);
+  fill('yellow');
+  noStroke();
+  // image(tileImgs[0], mouseX, mouseY, 50, 50);
+  circle(mouseX, mouseY, 50);
+
+  tiles.forEach((aTile) => {
+    aTile.render(tileImgs);
+  });
+}
