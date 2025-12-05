86f40aa257568305388ff6af3481d8f360356b1c
chaei <209975817+chaei2@users.noreply.github.com>
2025-12-05 12:15:12 +0900

    저장

--- src/w12/step2/sketch/main.js
+++ src/w12/step2/sketch/main.js
@@ -1,65 +1,103 @@
 const sketchContainer = document.querySelector('#sketch-container');
 
-function setup() {
-  createCanvas(600, 600);
-  const renderer = createCanvas(600, 600);
-  renderer.parent(sketchContainer);
+// 엇갈리는 선을 바꿀려면 blendLines을 바꾸면 됨
+function blendLines(
+  blendNums = [1],
+  lineBegins = [0, 0, 0, height],
+  lineEnds = [width, 0, width, height],
+  colours = ['black']
+) {
+  // blendNum.length으로 쓰면 blendNum도 어래이로 바꿔달라는 것임
+  for (let c = 0; c < blendNums.length; c++) {
+    const blendNum = blendNums[c];
+    const cBegin = 4 * c;
+    const lineBegin = [
+      lineBegins[cBegin],
+      lineBegins[cBegin + 1],
+      lineBegins[cBegin + 2],
+      lineBegins[cBegin + 3],
+    ];
+    const lineEnd = [
+      lineEnds[cBegin],
+      lineEnds[cBegin + 1],
+      lineEnds[cBegin + 2],
+      lineEnds[cBegin + 3],
+    ];
 
-  // 0.5 * width여기에서의 n * width 부분에 대한 값을 정해야함
-  strokeWidth = (0.5 * width) / strokeNum - 1;
+    const colour = colours[c];
+
+    const [bx1, by1, bx2, by2] = lineBegin;
+    const [ex1, ey1, ex2, ey2] = lineEnd;
+    stroke(colour);
+    for (let n = 0; n < blendNum + 2; n++) {
+      //  blendNum 0 부터 1까지
+      const t = n / (blendNum + 1);
+
+      // bx1 + (ex1 - bx1) * t이 방식은 널프?라는 방식임 시작값, 그리고 끝값을
+      // 시작 + t * (끝 -시작) 으로 하면 시작일떄 0, 끝일떄 1나옴
+      const x1 = bx1 + (ex1 - bx1) * t;
+      const y1 = by1 + (ey1 - by1) * t;
+      const x2 = bx2 + (ex2 - bx2) * t;
+      const y2 = by2 + (ey2 - by2) * t;
+      line(x1, y1, x2, y2);
+    }
+  }
+
+  // const [bx1, by1, bx2, by2] = lineBegin;
+  // const [ex1, ey1, ex2, ey2] = lineEnd;
+  // stroke(colour);
+  // for (let n = 0; n < blendNum + 2; n++) {
+  //   //  blendNum 0 부터 1까지
+  //   const t = n / (blendNum + 1);
+
+  //   // bx1 + (ex1 - bx1) * t이 방식은 널프?라는 방식임 시작값, 그리고 끝값을
+  //   // 시작 + t * (끝 -시작) 으로 하면 시작일떄 0, 끝일떄 1나옴
+  //   const x1 = bx1 + (ex1 - bx1) * t;
+  //   const y1 = by1 + (ey1 - by1) * t;
+  //   const x2 = bx2 + (ex2 - bx2) * t;
+  //   const y2 = by2 + (ey2 - by2) * t;
+  //   line(x1, y1, x2, y2);
+  // }
 }
 
-// n * W - W = 총 선 면적 = 를 t로 정의
-// stroke 두께에 따른
+function setup() {
+  // createCanvas(600, 600);
+  const renderer = createCanvas(600, 600);
+  renderer.parent(sketchContainer);
+}
 
-// 이름 곂치면 터짐
-let strokeNum = 50;
-let strokeWidth;
-let seed = 1;
+let bx1offset = 0;
+let bx2offset = 0;
+let ex1offset = 0;
+let ex2offset = 0;
+const offsetRange = 20;
+let seed = 0;
 
 function draw() {
   randomSeed(seed);
+  bx1offset = random(-offsetRange, offsetRange);
+  bx2offset = random(-offsetRange, offsetRange);
+  ex1offset = random(-offsetRange, offsetRange);
+  ex2offset = random(-offsetRange, offsetRange);
 
   background(255);
-  stroke('#00a6ffff');
-
-  strokeWeight(strokeWidth);
-  drawPattern(strokeNum, [random(2 * strokeWidth), 0]);
-  // 발광하는 걸 randomseed로 조금 괜찮게 바꿀 수 있음
-  stroke('#8F0177');
-
-  for (let n = 0; n < strokeNum; n++) {
-    // 임의로 만든 변수들
-
-    // x끼리는 값이 똑같아야함 y끼리는 값이 달라야 1자로 선이 만들어 질 것임
-    // normalizing => 보통   const t = n / strokeNum이라고 함
-    // 50이 되지 못함
-    // (strokeNum - 1)처리를 해줘야 이 num은 stroke 1이랑 숫자가 같아지지 않기 때문임
-    const t = n / (strokeNum - 1);
-    const x1 = width * t;
-    const x2 = x1;
-    const y1 = 0;
-    const y2 = height;
-    line(x1, y1, x2, y2);
-  }
+  strokeWeight(7);
+  blendLines(
+    [50, 51],
+    [0 + bx1offset, 0, 0 + bx2offset, height, 0, 0, 0, height],
+    [width + ex1offset, 0, width + ex2offset, height, width, 0, width, height],
+    ['#360185', '#8F0177']
+  );
+  // stroke('#360185');
+  // blendLines(
+  //   50,
+  //   [0 + bx1offset, 0 + by2offset, -10, height],
+  //   [width + ex1offset, 0, width + ey2offset, height]
+  // );
+  // stroke('#F4B342');
+  // blendLines(51);
 }
 
-function drawPattern(strokeNum = 2, begin = [0, 0], size = [width, height]) {
-  // 실수로 0을 넣었을때, 발생하는 이슈를 다루기 위한 거임 만약에 0이면 빨리 반환하는 코드
-  if (strokeNum <= 1) return;
-
-  const [bx, by] = begin;
-  const [w, h] = size;
-
-  for (let n = 0; n < strokeNum; n++) {
-    const t = n / (strokeNum - 1);
-    const x1 = w * t + bx;
-
-    // bx가 또 더해지니깐
-    const x2 = x1;
-    // const x2 = x1 + bx;
-    const y1 = 0 + by;
-    const y2 = h + by;
-    line(x1, y1, x2, y2);
-  }
+function mousePressed() {
+  seed++;
 }
